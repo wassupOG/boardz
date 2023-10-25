1798f500b714eadc477fb1669730c400b5d813a2
Ilya Naumov <112390560+wassupOG@users.noreply.github.com>
2023-10-25 12:53:10 +0300

    db basics

---
+++ app/(server)/db-actions.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import prisma from "@/db"
+
+export type Task = {
+  id?: number
+  title: string
+  description: string
+}
+
+export async function createTask({ title, description }: Task) {
+  await prisma.task.create({ data: { title: title, description: description } })
+}
+
+export async function getTasks() {
+  return await prisma.task.findMany()
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { Task, createTask, getTasks } from "./(server)/db-actions"
+import { useEffect, useState } from "react"
+import { Card } from "@/components/ui/card"
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getTasks().then((data) => setTasks(data))
+    setLoading(false)
+  }, [])
+
+  if (loading) {
+    return "Loading..."
+  }
+
   return (
     <>
-      <h1 className="p-4 py-2 text-3xl">Hellew, boardz!</h1>
-      <Button>Button</Button>
+      <Card className="p-5">
+        <h1 className="text-center text-3xl">Planned 🕐</h1>
+        {tasks.map((task) => (
+          <div>
+            {task.title} | {task.description}
+          </div>
+        ))}
+      </Card>
     </>
   )
 }

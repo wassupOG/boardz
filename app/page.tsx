2aa017172d01b4a0f1ace1c01cdac20487c1678f
Ilya Naumov <112390560+wassupOG@users.noreply.github.com>
2023-10-25 21:46:00 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,35 +7,6 @@ import { Task, getTasks } from "./db-actions"
 
 export type TaskType = "planned" | "progress" | "done"
 
-export type Tasks = {
-  id: number
-  title: string
-  description: string
-  type: TaskType
-}
-
-export const tasks: Tasks[] = [
-  {
-    id: 1,
-    title: "First task",
-    description: "Description of the first task",
-    type: "planned",
-  },
-  {
-    id: 2,
-    title: "Second task",
-    description: "Description of the second task",
-    type: "progress",
-  },
-  {
-    id: 3,
-    title: "Third task's title is longer",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolorem dolor fuga necessitatibus distinctio in consequuntur tenetur aspernatur obcaecati. Minima rem molestias et! Expedita, neque aut asperiores cumque incidunt deleniti!",
-    type: "done",
-  },
-]
-
 export default function Home() {
   const [tasks, setTasks] = useState<Task[] | null>(null)
 
@@ -48,8 +19,8 @@ export default function Home() {
   } else {
     return (
       <>
-        <DesktopBoards tasks={tasks as Task[]} />
-        <MobileBoards tasks={tasks as Task[]} />
+        <DesktopBoards tasks={tasks} />
+        <MobileBoards tasks={tasks} />
       </>
     )
   }
